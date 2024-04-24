b8c9f235579230aea33174054c765052ddbef0ca
Karimnazarov Jamshed <karimnazarovj@gmail.com>
2024-04-24 17:54:38 -0500

    Add start time saving functionality to MainSearchTask and MainChatTask

--- src/bing/MainSearchTask.js
+++ src/bing/MainSearchTask.js
@@ -1,16 +1,36 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Navbar from "../chat/navbar";
 import NoteBar from "../chat/NoteBar";
 import TaskContext from "../context/task-context";
 import EndTaskPopUp from "../chat/EndTaskPopUp";
 import SearchPage from "./SearchPage";
+import AuthContext from "../context/auth-context";
 import InstructionsPopUp from "../questionnaire/InstructionsPopUp";
+import { Timestamp, setDoc, doc } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const MainSearchTask = () => {
   const [showInstructions, setShowInstructions] = useState(true);
+  const authCtx = useContext(AuthContext);
   const taskCtx = useContext(TaskContext);
   const instructionText =
     "Respond to the 'Current task' on the left, using 'Search the web...' for research. Evaluate each result, compile your answer in the right-side note box, and submit when ready";
+  // save the start time
+  useEffect(() => {
+    const saveStartTime = async () => {
+      try {
+        const startTime = Timestamp.now();
+        const userDocRef = doc(db, "searchTask", authCtx.user.uid);
+        await setDoc(userDocRef, { startedTs: startTime }, { merge: true });
+        console.log("Start time saved");
+      } catch (error) {
+        console.error("Error saving start time:", error);
+      }
+    };
+
+    saveStartTime();
+  }, [authCtx.user]);
+
   return (
     <div className="flex flex-row w-screen">
       <Navbar setShowInstructions={setShowInstructions} />
--- src/chat/MainChatTask.js
+++ src/chat/MainChatTask.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import Navbar from "./navbar";
 import Chatbox from "./chatbox";
 import NoteBar from "./NoteBar";
@@ -6,13 +6,32 @@ import TaskContext from "../context/task-context";
 import EndTaskPopUp from "./EndTaskPopUp";
 import MainSearchPage from "../bing/SearchPage";
 import InstructionsPopUp from "../questionnaire/InstructionsPopUp";
+import { Timestamp, setDoc, doc } from "firebase/firestore";
+import AuthContext from "../context/auth-context";
+import { db } from "../firebase-config";
 
 const MainChatTask = () => {
   const [showInstructions, setShowInstructions] = useState(true);
+  const authCtx = useContext(AuthContext);
   const taskCtx = useContext(TaskContext);
 
   const instructionText =
     "Your task is under 'Current Task' on the left. Interact with ChatGPT using 'Type a prompt...' and compile your answer in the note box on the right before submitting.";
+
+  useEffect(() => {
+    const saveStartTime = async () => {
+      try {
+        const startTime = Timestamp.now();
+        const userDocRef = doc(db, "chatTasks", authCtx.user.uid);
+        await setDoc(userDocRef, { startedTs: startTime }, { merge: true });
+        console.log("Start time saved");
+      } catch (error) {
+        console.error("Error saving start time:", error);
+      }
+    };
+
+    saveStartTime();
+  }, [authCtx.user]);
 
   return (
     <div className="flex flex-row bg-[#e3e3e3] w-screen border-2 border-red-600">
