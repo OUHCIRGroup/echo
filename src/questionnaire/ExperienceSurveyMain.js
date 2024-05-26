2886ea932a4edf4c0f91a19cd0b0650b95a77d81
Karimnazarov Jamshed <karimnazarovj@gmail.com>
2024-05-26 10:51:40 -0500

    Added topic fimilarty question in experience survey

--- src/questionnaire/ExperienceSurveyMain.js
+++ src/questionnaire/ExperienceSurveyMain.js
@@ -26,6 +26,7 @@ const ExperienceSurveyMain = () => {
     overallExperience: "",
     responseSatisfaction: "",
     objectiveAchievement: "",
+    topicFamiliarity: "",
     additionalComments: "",
     threeExamples: ["", "", ""],
   });
@@ -68,6 +69,7 @@ const ExperienceSurveyMain = () => {
             overallExperience: restult.overallExperience,
             responseSatisfaction: restult.responseSatisfaction,
             objectiveAchievement: restult.objectiveAchievement,
+            topicFamiliarity: restult.topicFamiliarity,
             additionalComments: restult.additionalComments,
             threeExamples: restult.threeExamples || ["", "", ""],
           });
